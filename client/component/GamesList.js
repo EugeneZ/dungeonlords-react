f8f3c7dbc3690504d3a493c36fd4d35a227273ce
Eugene Zaretskiy <eugene.zar@gmail.com>
2015-01-29 21:10:33 -0500

    Barebones game list just for development

--- client/component/GamesList.js
+++ client/component/GamesList.js
@@ -3,18 +3,40 @@
 var React = require('react'),
     FluxMixin = require('fluxxor').FluxMixin(React),
     StoreWatchMixin = require('fluxxor').StoreWatchMixin,
+    Link = require('react-router').Link,
     NewGame = require('./NewGame');
 
 module.exports = React.createClass({
     mixins: [FluxMixin],
+    getInitialState: function(){
+        var userStore = this.getFlux().store('Users');
+
+        return {
+            me: userStore.getLoggedInUser()
+        };
+    },
     render: function () {
-        if (true) {
+        if (!this.state.me.games || !this.state.me.games.length) {
             return <NewGame/>
         }
+
+        var games = this.state.me.games.map(function(game){
+            return (
+                <tr>
+                    <td><Link to={'/games/' + game}>{game}</Link></td>
+                </tr>
+            );
+        });
+
         return (
             <div className="row">
                 <div className="col-sm-12">
                     <h2>Games</h2>
+                    <table className="table">
+                        <tbody>
+                            {games}
+                        </tbody>
+                    </table>
                 </div>
             </div>
         );
